778e3825ae7559eadb63c007fe96c463c05aab6c
Paddy Drury <paddy.e.drury@gmail.com>
2021-12-01 16:28:52 +0000

    tidy

--- src/main/kotlin/day01/Day1.kt
+++ src/main/kotlin/day01/Day1.kt
@@ -3,19 +3,16 @@ package day01
 import util.loadInputFromResource
 
 fun main(args: Array<String>) {
-    println("Part 1 test ${Day1("test").part1()}")
+    println("Part 1 test   ${Day1("test").part1()}")
     println("Part 1 actual ${Day1("input").part1()}")
-    println("Part 2 test ${Day1("test").part2()}")
+    println("Part 2 test   ${Day1("test").part2()}")
     println("Part 2 actual ${Day1("input").part2()}")
 }
 
 class Day1(
     inputName: String,
     private val input: List<Int> = loadInputFromResource<Day1>(inputName).map { it.toInt() }) {
-
-
-
-    fun part1() = input.zipWithNext().count { it.second > it.first }
-
-    fun part2() = input.windowed(size = 3).map { it.sum() }.zipWithNext().count { it.second > it.first }
+    fun part1() = countIncreases(input)
+    fun part2() = countIncreases(input.windowed(size = 3).map { it.sum() })
+    private fun countIncreases(list: List<Int>) = list.zipWithNext().count { it.second > it.first }
 }
